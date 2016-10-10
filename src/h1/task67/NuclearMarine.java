7ea20147c3e36a89549d0b5ab0a0c73f504c9797
MaximKirko <30max30max@gmail.com>
2016-10-10 11:04:15 +0300

    add task 6

---
+++ src/h1/task67/NuclearMarine.java
@@ -0,0 +1,30 @@
+package h1.task67;
+
+public class NuclearMarine {
+	private String name;
+	private double speed;
+	private int crew;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public int getCrew() {
+		return crew;
+	}
+	public void setCrew(int crew) {
+		this.crew = crew;
+	}
+	
+	public NuclearMarine() {
+		
+	}
+}
